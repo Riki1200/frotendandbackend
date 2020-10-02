ac46d325c941664504bc2b55e25019155830e45c
Riki1200 <betances002@gmail.com>
2020-10-01 22:19:33 -0400

    add context theme

---
+++ frontend/src/context/useContextTheme.jsx
@@ -0,0 +1,28 @@
+import React, { useState, useContext } from 'react';
+
+
+const ContextTheme = React.createContext('blue_ligth');
+const ContextUpdateTheme = React.createContext();
+
+
+export const useTheme = () => useContext(ContextTheme);
+export const useThemeUpdate = () => useContext(ContextUpdateTheme);
+
+export const useThemeContext = ({ children }) => {
+
+    const [theme, setTheme] = useState(false);
+
+    const BlueLight = {
+        backgroundColor: changeColor ? '#2CA2FF' : '#000068',
+        color: changeColor ? '#000' : '#fff'
+    }
+
+    return <ContextTheme.Provider value={theme, BlueLight}>
+        <ContextUpdateTheme.Provider value={setTheme}>
+            {children}
+        </ContextUpdateTheme.Provider>
+    </ContextTheme.Provider>
+
+}
+
+
